0aa26bb5653413502942ade5f8b7ced8ad730492
Merissa <53382622+merissali@users.noreply.github.com>
2026-09-10 20:13:05 -0700

    Update next.config.ts
    remove basepath variable

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const isGitHubPages = process.env.PERSONA_STATIC_EXPORT === "true";
-const githubPagesBasePath = process.env.GITHUB_PAGES_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
   ...(isGitHubPages
     ? {
         output: "export",
         images: { unoptimized: true },
-        // basePath: githubPagesBasePath,
       }
     : {}),
 };
